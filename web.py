aae318ce5a4fc77b9e6046871754759457e6b751
Sheel2007 <sheel_arsenal@icloud.com>
2022-09-22 16:48:48 -0400

    Fixed Local Port Bug

--- web.py
+++ web.py
@@ -2,10 +2,14 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "<h1>This is the home page</h1>"
+
 @app.route("/<name>")
-def home(name):
+def name(name):
     return render_template("index.html", content=name)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=4096)
